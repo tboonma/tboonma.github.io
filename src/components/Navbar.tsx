541a50ca4d47cd930d36e2fbb56decacb3e76fd6
tboonma <tboonma@hotmail.com>
2022-12-01 13:43:44 +0700

    Added navbar transition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,23 @@
 import './Navbar.css'
 import { HiBars3BottomRight } from 'react-icons/hi2'
+import { useEffect, useState } from 'react'
 
 const Navbar = () => {
+  const [showBackground, setBackground] = useState<boolean>(false)
+
+  const changeBackgroundColor = () => {
+    setBackground(window.scrollY >= 110)
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', changeBackgroundColor)
+  })
   return (
-    <div className="Navbar px-10 py-8 flex justify-between w-full fixed">
+    <div
+      className={
+        `Navbar px-10 py-8 flex justify-between w-full fixed duration-300 ` +
+        (showBackground ? 'bg-white shadow-md ' : '')
+      }
+    >
       <h1 className="text-xl title text-center">Tawan B.</h1>
       <div className="text-center text-xl hidden md:flex space-x-14 text-custom-blue">
         <h2 className="">
