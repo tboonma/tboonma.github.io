c95dc149cb11bf1665b4bdfdfe8278478b2e8d0e
tboonma <tboonma@hotmail.com>
2022-12-03 01:17:41 +0700

    removed unused

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -5,7 +5,6 @@ import {
 } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
 import { MdWork } from 'react-icons/md'
-import AgodaIcon from '../assets/img/agoda.jpg'
 
 export interface WorkExperience {
   positionName: string
