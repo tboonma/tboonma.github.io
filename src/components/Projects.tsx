89a82199d5c6b7a1c70ea4767edd4baed3af9987
tboonma <tboonma@hotmail.com>
2022-12-03 03:06:33 +0700

    Updated project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import './Projects.css'
+import { SiPython } from 'react-icons/si'
 
 const Projects = () => {
+  const GetAllRepo = async () => {
+    const response = await axios.get(
+      'https://api.github.com/users/tboonma/repos?&per_page=100&sort=pushed'
+    )
+    console.log(response.data)
+  }
+
+  useEffect(() => {
+    GetAllRepo()
+  }, [])
+
   return (
     <div id="projects" className="Projects pt-28 px-10">
       <h2
         id="title"
-        className="text-custom-blue text-3xl md:text-4xl text-center pt-24 pb-10 sha"
+        className="text-custom-blue text-3xl md:text-4xl text-center pt-24 pb-10"
       >
         Projects
       </h2>
+      <div className="grid md:grid-cols-3 gap-5">
+        <div className="bg-translucent shadow-md p-3 rounded-xl">
+          <p className="text-xl font-bold text-custom-blue2">Project name</p>
+        </div>
+        <div className="bg-translucent shadow-md p-3 rounded-xl">Hello</div>
+        <div className="bg-translucent shadow-md p-3 rounded-xl">Hello</div>
+        <div className="bg-translucent shadow-md p-3 rounded-xl">Hello</div>
+      </div>
     </div>
   )
 }
