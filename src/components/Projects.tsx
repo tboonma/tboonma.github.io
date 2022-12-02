297d404f5f65874ab424c3715e27ea8028779323
tboonma <tboonma@hotmail.com>
2022-12-03 01:31:50 +0700

    added title for projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div id="projects" className="Projects pt-28">
-      Projects
+    <div id="projects" className="Projects pt-28 px-10">
+      <h2
+        id="title"
+        className="text-custom-blue text-3xl md:text-4xl text-center pt-24 pb-10 sha"
+      >
+        Projects
+      </h2>
     </div>
   )
 }
