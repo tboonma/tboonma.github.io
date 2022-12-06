4f3ddbb9c74011a52615f6c8f730027409848ac7
tboonma <tboonma@hotmail.com>
2022-12-07 02:43:24 +0700

    Improved navigation bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import './Navbar.css'
 import { HiBars3BottomRight } from 'react-icons/hi2'
 import { ChangeEvent, useEffect, useState } from 'react'
-import { MdOutlineDarkMode, MdOutlineWbSunny } from 'react-icons/md'
+import { MdOutlineDarkMode, MdOutlineWbSunny, MdClose } from 'react-icons/md'
 import { inject, observer } from 'mobx-react'
 import { mobXStore } from '../stores/MobXstore'
+import React from 'react'
 
 const Navbar = () => {
   const store = mobXStore
   const [showBackground, setBackground] = useState<boolean>(false)
+  const [showNavbarInMobile, setNavbarInMobile] = useState<boolean>(false)
+  const box = React.createRef<any>()
 
   const changeThemeHandler = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault()
@@ -18,6 +21,17 @@ const Navbar = () => {
     setBackground(window.scrollY >= 80)
   }
 
+  const openNavbar = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault()
+    setNavbarInMobile(true)
+  }
+
+  const closeNavbar = (event?: React.MouseEvent<HTMLElement>) => {
+    event?.preventDefault()
+    console.log(1)
+    setNavbarInMobile(false)
+  }
+
   useEffect(() => {
     window.addEventListener('scroll', changeBackgroundColor)
   })
@@ -25,20 +39,44 @@ const Navbar = () => {
   return (
     <div
       className={
-        `Navbar px-10 py-6 flex justify-between w-full fixed duration-300` +
-        (showBackground ? ' dark:bg-gray-shark bg-white shadow-md ' : '')
+        `Navbar px-10 py-6 w-full fixed duration-300` +
+        (showBackground || showNavbarInMobile
+          ? ' dark:bg-gray-shark bg-white shadow-md'
+          : '') +
+        (showNavbarInMobile ? ' h-full' : ' flex justify-between')
       }
     >
-      <div className="title grid place-content-start lg:place-content-center w-2/6 lg:w-1/6">
+      <div
+        className={
+          'title lg:w-1/6' +
+          (showNavbarInMobile
+            ? ' w-full flex justify-between items-center'
+            : ' w-2/6 grid place-content-start lg:place-content-center')
+        }
+      >
         <div className="text-xl text-center text-blue-light dark:text-blue-dark">
           Tawan B.
         </div>
+        <div
+          className={'cursor-pointer' + (showNavbarInMobile ? '' : ' hidden')}
+          onClick={closeNavbar}
+        >
+          <MdClose className="w-6 h-6 text-white-seashell" />
+        </div>
       </div>
-      <div className="text-center hidden lg:flex lg:items-center lg:justify-between w-3/6 px-10">
+      <div
+        className={
+          'text-center lg:flex lg:items-center lg:justify-between px-10' +
+          (showNavbarInMobile
+            ? ' w-full mt-8 space-y-8 text-2xl'
+            : ' hidden w-3/6')
+        }
+      >
         <h2>
           <a
             href="#about-me"
             className="text-clay dark:text-white-seashell hover:text-slate-500 font-medium"
+            ref={box}
           >
             About me
           </a>
@@ -88,7 +126,13 @@ const Navbar = () => {
           Resume
         </button>
       </div>
-      <div className="grid place-content-center lg:hidden">
+      <div
+        className={
+          'place-content-center lg:hidden' +
+          (showNavbarInMobile ? ' hidden' : ' grid')
+        }
+        onClick={openNavbar}
+      >
         <HiBars3BottomRight className="w-6 h-6 cursor-pointer text-clay dark:text-white-seashell" />
       </div>
     </div>
