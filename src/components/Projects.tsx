6d744d18916824977b2dfa05997ab2eb82a0945c
tboonma <tboonma@hotmail.com>
2022-12-12 14:29:06 +0700

    extend padding

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,7 +17,7 @@ const Projects = () => {
   }, [])
 
   return (
-    <div id="projects" className="Projects pt-24 px-10">
+    <div id="projects" className="Projects py-24 px-10">
       <div className="space-y-3 md:col-span-3 text-center md:text-left">
         <div className="before:bg-gray-alto heading text-gray-nevada text-sm md:text-md tracking-extrawide flex items-center justify-center md:justify-start">
           <p>MY WORKS</p>
