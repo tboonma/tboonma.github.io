96af4312424512f66b3f237b70d493b1f163a039
tboonma <tboonma@hotmail.com>
2022-12-14 13:34:11 +0700

    Relocate file

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import AboutMe from '../../components/AboutMe'
+import Certificates from '../../components/Certificates'
+import Contact from '../../components/Contact'
+import Education from '../../components/Education'
+import Experiences from '../../components/Experiences'
+import Projects from '../../components/Projects'
+
+const Home = () => {
+  return (
+    <div>
+      <AboutMe />
+      <Experiences />
+      <Projects />
+      <Education />
+      <Certificates />
+      <Contact />
+    </div>
+  )
+}
+
+export default Home
