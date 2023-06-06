98850cd1957319933270fab6056a594dc7c537cc
tboonma <tboonma@hotmail.com>
2023-06-06 13:22:31 +0900

    Added 404 page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './css/style.css'
+
+const NotFound = () => {
+  return (
+    <div id="notfound">
+		<div className="notfound">
+			<div className="notfound-404">
+				<h1>404</h1>
+			</div>
+			<h2>Oops! This Page Could Not Be Found</h2>
+			<p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+			<a href="/">Go To Homepage</a>
+		</div>
+	</div>
+    // This templates was made by Colorlib (https://colorlib.com)
+  )
+}
+
+export default NotFound
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,11 @@
 import App from "@src/App";
+import NotFound from "@src/pages/NotFound";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router  = createBrowserRouter([
     {
         path: '/',
         element: <App />,
-        errorElement: <div>404 Not Found</div>
+        errorElement: <NotFound />
     }
 ])
