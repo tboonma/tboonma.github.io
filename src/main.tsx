d8b8332e65c3e374365f6c756bc7ac9c888f57a5
tboonma <tboonma@hotmail.com>
2023-06-06 13:08:45 +0900

    Added 404 Not Found

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import { Provider } from 'mobx-react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import { mobXStore } from './stores/MobXStore'
 import { Analytics } from '@vercel/analytics/react'
+import { router } from './router'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider {...{ mobXStore }}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
     {import.meta.env.PROD ? <Analytics /> : <></>}
   </React.StrictMode>
